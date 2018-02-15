b757529fcf2cab875ba7644d685ddc6b5dcb812a
Tamo <Tamo@TAMO-PC>
2018-02-15 17:47:18 +0400

    Add project files.

---
+++ SecretPicture/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace SecretPicture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonshowPicture1 = new System.Windows.Forms.Button();
+            this.buttonshowpicture2 = new System.Windows.Forms.Button();
+            this.buttonshowpicture3 = new System.Windows.Forms.Button();
+            this.buttonshowpicture4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonshowPicture1
+            // 
+            this.buttonshowPicture1.BackColor = System.Drawing.SystemColors.Control;
+            this.buttonshowPicture1.Font = new System.Drawing.Font("Lucida Sans Unicode", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonshowPicture1.Location = new System.Drawing.Point(12, 12);
+            this.buttonshowPicture1.Name = "buttonshowPicture1";
+            this.buttonshowPicture1.Size = new System.Drawing.Size(75, 23);
+            this.buttonshowPicture1.TabIndex = 0;
+            this.buttonshowPicture1.Text = "field";
+            this.buttonshowPicture1.UseVisualStyleBackColor = false;
+            this.buttonshowPicture1.Click += new System.EventHandler(this.buttonshowPicture1_Click);
+            // 
+            // buttonshowpicture2
+            // 
+            this.buttonshowpicture2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonshowpicture2.Font = new System.Drawing.Font("Lucida Sans Unicode", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonshowpicture2.Location = new System.Drawing.Point(297, 12);
+            this.buttonshowpicture2.Name = "buttonshowpicture2";
+            this.buttonshowpicture2.Size = new System.Drawing.Size(75, 23);
+            this.buttonshowpicture2.TabIndex = 1;
+            this.buttonshowpicture2.Text = "lake";
+            this.buttonshowpicture2.UseVisualStyleBackColor = true;
+            this.buttonshowpicture2.Click += new System.EventHandler(this.buttonshowpicture3_Click);
+            // 
+            // buttonshowpicture3
+            // 
+            this.buttonshowpicture3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonshowpicture3.Font = new System.Drawing.Font("Lucida Sans Unicode", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonshowpicture3.Location = new System.Drawing.Point(12, 210);
+            this.buttonshowpicture3.Name = "buttonshowpicture3";
+            this.buttonshowpicture3.Size = new System.Drawing.Size(75, 23);
+            this.buttonshowpicture3.TabIndex = 2;
+            this.buttonshowpicture3.Text = "river";
+            this.buttonshowpicture3.UseVisualStyleBackColor = true;
+            this.buttonshowpicture3.Click += new System.EventHandler(this.buttonshowpicture3_Click_1);
+            // 
+            // buttonshowpicture4
+            // 
+            this.buttonshowpicture4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonshowpicture4.Font = new System.Drawing.Font("Lucida Sans Unicode", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonshowpicture4.Location = new System.Drawing.Point(297, 210);
+            this.buttonshowpicture4.Name = "buttonshowpicture4";
+            this.buttonshowpicture4.Size = new System.Drawing.Size(75, 23);
+            this.buttonshowpicture4.TabIndex = 3;
+            this.buttonshowpicture4.Text = "road";
+            this.buttonshowpicture4.UseVisualStyleBackColor = true;
+            this.buttonshowpicture4.Click += new System.EventHandler(this.buttonshowpicture4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.buttonshowpicture4);
+            this.Controls.Add(this.buttonshowpicture3);
+            this.Controls.Add(this.buttonshowpicture2);
+            this.Controls.Add(this.buttonshowPicture1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "The picture with a secret";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonshowPicture1;
+        private System.Windows.Forms.Button buttonshowpicture2;
+        private System.Windows.Forms.Button buttonshowpicture3;
+        private System.Windows.Forms.Button buttonshowpicture4;
+    }
+}
+
---
+++ SecretPicture/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SecretPicture
+{
+    public partial class Form1 : Form
+    {
+        int secret_nr = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void buttonshowPicture1_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Properties.Resources.picture1;
+        }
+
+        private void buttonshowpicture4_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Properties.Resources.picture4;
+        }
+
+        private void buttonshowpicture3_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Properties.Resources.picture2;
+        }
+
+        private void buttonshowpicture3_Click_1(object sender, EventArgs e)
+        {
+            if (secret_nr == 2)
+                this.BackgroundImage = Properties.Resources.secret;
+            else
+
+                this.BackgroundImage = Properties.Resources.picture3;
+            secret_nr = 0;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+                secret_nr = 1;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (secret_nr == 1)
+                if (e.X < buttonshowPicture1.Location.X &&
+                    e.Y < buttonshowPicture1.Location.Y)
+                {
+                    secret_nr = 2;
+                }
+
+        }
+    }
+}
+
